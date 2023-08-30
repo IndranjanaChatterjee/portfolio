3961d46200a98784ba2131f1236db483ec6f89ac
Indranjana <112919974+IndranjanaChatterjee@users.noreply.github.com>
2023-08-30 20:19:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#element", {
+  strings: [
+    "Passionate Web developer","Copetitive Coder","Coding Enthusiast",
+  ],
+  typeSpeed: 100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+});
